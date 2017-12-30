403875cd5e30d11f93a7b20a2cbdd5813ce55f60
Arnaud Casamé <djcase001@gmail.com>
2017-12-30 03:56:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  
+  object[key] = value;
+  return object;
 }
 
 function deleteFromObjectByKey(object, key){
   var newObj = Object.assign({}, object);
   delete newObj.key;
  
-  return newObj
-  
+  return newObj;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
